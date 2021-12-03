b807816b657d738ede4bc9f2a3bf99c4e26098ec
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2021-12-03 16:15:18 +0100

    update main

--- solution-2/src/main.rs
+++ solution-2/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
+
+fn read<R: Read>(io: R) -> Result<Vec<String>, Error> {
+    let br = BufReader::new(io);
+    br.lines()
+        .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
+        .collect()
+}
+
+
+fn main() -> Result<(), Error> {
+    let vec = read(File::open("input2")?)?;
+    let mut stringVec = vec.clone(); 
+
+    let mut currentCount = 0;
+
+    // Add counting logic
+    // String in vector and addition in another
+
+    for x in vec.iter() {
+        x.split_whitespace();
+        println!("x={}, currentCount={}", x, currentCount);
+    }
+
+
+    Ok(())
 }
