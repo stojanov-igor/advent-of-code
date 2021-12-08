0ebba8aaeb3fed6fd70eae034883cf7e20184108
JosephKnecht-lab <authorJosephKnecht@gmail.com>
2021-12-08 20:34:40 +0100

    update

--- solution-2/src/main.rs
+++ solution-2/src/main.rs
@@ -17,11 +17,29 @@ fn main() -> Result<(), Error> {
 
     // Add counting logic
     // String in vector and addition in another
-    
+    let mut forward_count= 0;
+    let mut down_count = 0;
+    let mut up_count = 0;
+    let mut number_end = 0;
 
     for x in vec.iter() {
-        x.split_whitespace();
-        println!("x={}, currentCount={}", x, currentCount);
+        //let y = x.split_whitespace();
+        let char_vec: Vec<char> = x.chars().collect();
+        let number_end: Vec<u32> = x.chars().rev().take(1).flat_map(|ch| ch.to_digit(10)).collect();
+
+        let ch = char_vec[0];
+        if ch == 'f' {
+            forward_count += 1;
+            // number_end += number_end;
+        }
+        if ch == 'd' {
+            down_count += 1;
+        }
+        if ch == 'u' {
+            up_count += 1;
+        }
+
+        println!("forward={:?}, down={:?}, up={:?}, Total_forward: {:?}",  forward_count, down_count, up_count, number_end);
     }
 
 
